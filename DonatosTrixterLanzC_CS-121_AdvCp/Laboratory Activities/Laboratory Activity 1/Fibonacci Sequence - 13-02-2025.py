1f11e60223a334b17a320ff72676f61b8c0c5ee9
Grizzly-val <lanz.10donatos@gmail.com>
2025-02-13 11:20:38 +0800

    Add files via upload

---
+++ DonatosTrixterLanzC_CS-121_AdvCp/Laboratory Activities/Laboratory Activity 1/Fibonacci Sequence - 13-02-2025.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number of terms: "))
+first = 0
+second = 1
+
+print("Fibonacci Sequence: ", end=' ')
+for i in range(n):
+    print(first, end=' ')
+    first, second = second, first + second
